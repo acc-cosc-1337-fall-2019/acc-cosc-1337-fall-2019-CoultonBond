59184d57ce2c16b7048f5cb2f447a5b07813ac75
Coulton Bond <coulton.bond@g.austincc.edu>
2019-09-25 19:25:58 -0500

    9/25/19 changes

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -27,6 +27,7 @@ and displays the numbers to screen using an indexed for loop as follows: 9 10 99
 */
 void loop_vector_w_index(std::vector<int> nums)
 {
+	nums[0] = 1000;
 	for (int i = 0; i < nums.size(); ++i)
 	{
 		std::cout << nums[i] << "\n";
@@ -38,8 +39,9 @@ Write code for function loop_vector_w_index with a vector of int pass by referen
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
-void loop_vector_w_index_ref(std::vector<int>& nums)
+void loop_vector_w_index_ref(std::vector<int>& nums)//pass by ref changes original vector value.
 {
+	nums[0] = 1000;
 	for (int i = 0; i < nums.size(); ++i)
 	{
 		std::cout << nums[i] << "\n";
@@ -60,10 +62,20 @@ void loop_vector_w_index_const(const std::vector<int>& nums)
 
 }
 
-void loop_vector_w_for_ranged(const std::vector<int>& nums)
+void loop_vector_w_for_ranged(std::vector<int>& nums)//does not change original values of vector.
 {
 	for (auto num : nums)
 	{
+		num = 1000;
+		std::cout << num << "\n";
+	}
+}
+
+void loop_vector_w_for_ranged_ref_var(std::vector<int>& nums)//changes original vector values.
+{
+	for (auto& num : nums)
+	{
+		num = 1000;
 		std::cout << num << "\n";
 	}
 }
