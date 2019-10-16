7ba338a7a82c40e03ca34d96415f86739e3e3a05
Coulton Bond <coulton.bond@g.austincc.edu>
2019-10-16 18:46:43 -0500

    Assignment 7 complete

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,23 +1,27 @@
 //bank_account.h
 #include<iostream>
-#ifndef BANK_ACCOUNT_H//header guards: prevent class redefinition error, need to use in tic-tac-toe
+
+#ifndef BANK_ACCOUNT_H//header guards-to prevent class redefinition error
 #define BANK_ACCOUNT_H
 class BankAccount
 {
 public:
-	BankAccount();//default constructor, same name as class
-	BankAccount(int b) : balance(b) {}; //inline constructor, parameters causes overloading
-	void deposit(int amount);
-	void withdraw(int amount); 
+	BankAccount();
+	BankAccount(int b) : balance(b) {};//inline constructor
 	int get_balance() const;//const makes class members read only
+	void deposit(int amount);
+	void withdraw(int amount);
+
 	friend void display(const BankAccount& account);
-	friend std::ostream & operator<< (std::ostream & out, const BankAccount & b);
-
+	friend std::ostream & operator << (std::ostream & out,
+		const BankAccount & b);
 	friend std::istream & operator >> (std::istream & in, BankAccount & b);
+	BankAccount operator+(const BankAccount& b);
+protected:
 
-private:
-	int balance;//balance is not accessible from outside the BankAccount class
 
+	int balance;
 
 };
+
 #endif
--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,28 +1,26 @@
 #include<iostream>
 #include<vector>
-#include "atm.h"
-#include "bank_account.h"
+#include"checking_account.h"
+#include "savings_account.h"
 
-using std::cin;
-using std::cout;
-using std::vector;
+using std::cout; using std::vector; using std::cin;
 
 int main()
 {
-	BankAccount a;//causes constructor to execute in bank_account.cpp
-	BankAccount b = a;//creates copy  
-	display(a);//call friend function, must be in the class but not part of the class
-	//scan card and enter pin
-	//get customer records from database into a vector
-	vector<BankAccount> accounts{ BankAccount(500), BankAccount(600), BankAccount(1000) };
-	BankAccount account(9000);
+	CheckingAccount a(1500);
+	cout << a;
 
-	ATM atm(accounts[0]);
-	atm.deposit(100);
-	atm.withdraw(50);
-	atm.display_balance();
-	cin >> a;
-	cout << a; //overload operator concept, function call behind the scenes
+	SavingsAccount savings(500);
+	cout << "Calls bank account overload ostream" << savings;
+	cout << "\n Savings get_balance" << savings.get_balance()<< "\n";
+	savings.add_interest();
+	cout << savings;
+
+	BankAccount c = a + savings; 
+
+
+	cout << c;
+
 
 	return 0;
 }
