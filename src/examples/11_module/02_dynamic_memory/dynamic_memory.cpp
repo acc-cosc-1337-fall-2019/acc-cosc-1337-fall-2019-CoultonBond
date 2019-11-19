880a3419ebe377cf57d523db9ce0776f6813bfeb
Coulton Bond <coulton.bond@g.austincc.edu>
2019-11-18 19:12:30 -0600

    Memory Examples

--- src/examples/11_module/02_dynamic_memory/dynamic_memory.cpp
+++ src/examples/11_module/02_dynamic_memory/dynamic_memory.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 //
 
+using std::cout;
+
 int* return_pointer(int number)
 {
 	return nullptr;
@@ -9,5 +11,11 @@ int* return_pointer(int number)
 
 void allocate_use_release()
 {
-	
+	int * intPtr; //declare
+	intPtr = new int(10); //asign
+	cout << *intPtr << "\n|"; //use
+	*intPtr = 100; //reassign
+	cout << *intPtr; //use
+	delete intPtr;
+
 }
